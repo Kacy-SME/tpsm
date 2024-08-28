de623f647f4b526e70f237cfc4c651da16bd659e
Juanjo Garcia Mesa <juanjosegarciamesa@gmail.com>
2024-08-28 16:02:07 -0700

    Pdominula data to assets; add url

--- scripts/setup_pdominula_data.sh
+++ scripts/setup_pdominula_data.sh
@@ -1,8 +1,8 @@
 
 # create data and p_dominula directories
-echo "Create p_dominula directories under data"
-mkdir -p data/p_dominula data/p_dominula_1080 data/p_dominula_256
-cd data/p_dominula
+echo "Create p_dominula directories under assets"
+mkdir -p assets/p_dominula assets/p_dominula_1080 assets/p_dominula_256
+cd assets/p_dominula
 
 # download p_dominula data
 url=$(head -n1 assets/pdominula_url.txt)
@@ -15,5 +15,7 @@ echo "Unzip p_dominula data"
 # crop videos to be square
 cd ..
 for video in $(ls p_dominula/*.mp4); do echo "Crop $(basename ${video}) to square 1080x1080"; ffmpeg -i ${video} -vf crop=1080:1080:420:0 -c:a copy p_dominula_1080/$(basename ${video}) -hide_banner -loglevel error; done
+
+# resize videos to be 256x256
 for video in $(ls p_dominula_1080/*.mp4); do echo "Resize $(basename ${video}) to 256x256"; ffmpeg -i ${video} -s 256x256 -c:a copy p_dominula_256/$(basename ${video}) -hide_banner -loglevel error; done
 
