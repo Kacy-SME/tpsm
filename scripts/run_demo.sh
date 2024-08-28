86f27b3d034ae0610fae2ec833c87e6d5f52ce3c
Juanjo Garcia Mesa <juanjosegarciamesa@gmail.com>
2024-08-28 16:08:12 -0700

    Update demo script

--- scripts/run_demo.sh
+++ scripts/run_demo.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-#SBATCH -p general
-#SBATCH -q public
-#SBATCH --time=0-00:05:00
-#SBATCH --gres=gpu:1g.10gb:1
+#SBATCH --partition=htc
+#SBATCH --qos=public
+#SBATCH --cpus-per-task=1
+#SBATCH --time=05:00
+#SBATCH --gres=gpu:1
 
 module load mamba/latest
-source activate tpsm
+source activate tpsm-wasps
 
+echo "Running the demo with the vox-256 checkpoint and the wasp source image and driving video."
+echo "Ensure the vox-256.pht.tar checkpoint is in the checkpoints directory"
+echo " (instructions https://github.com/yoyo-nb/Thin-Plate-Spline-Motion-Model)." 
 python demo.py --config config/vox-256.yaml --checkpoint checkpoints/vox.pth.tar --source_image assets/source.png --driving_video assets/driving.mp4
