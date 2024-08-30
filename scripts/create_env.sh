1fb6a5ecd8f2ebb7ac43027539c589980f7dd3cd
Juanjo Garcia Mesa <juanjosegarciamesa@gmail.com>
2024-08-30 11:49:20 -0700

    Fix creating python env

--- scripts/create_env.sh
+++ scripts/create_env.sh
@@ -3,11 +3,11 @@
 #SBATCH --job-name=tpsm_env
 #SBATCH --partition=htc
 #SBATCH --qos=public
-#SBATCH --cpus-per-task=1
-#SBATCH --time=30:00
+#SBATCH --cpus-per-task=4
+#SBATCH --time=0-1
 #SBATCH --mem=4G
 
 module load mamba/latest
 
 echo "Creating the tpsm-wasps environment."
-mamba create -y -f environment.yml
+conda env create -f reqs.yaml
